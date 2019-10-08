8155b6076b4733d4e37ef109204088755a7b2d55
Jordan Miller <jordan.j.miller@live.com>
2019-10-08 09:29:37 -0700

    Mvps step 2 complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { data } from '../src/data.js';
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -12,6 +13,9 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
+  const [numberState, setNumberState] = useState(data.numbers);
+  const [operatorState, setOperatorState] = useState(data.operators);
+  const [specialState, setSpecialState] = useState(data.specials);
 
   return (
     <div className="container">
